04283f3800096584c2235a89785ecf7c57c23126
bramplouvier <bramplouvier@gmail.com>
2022-07-15 15:41:32 +0200

    Update postDeploy.ps1

--- infra/postDeploy.ps1
+++ infra/postDeploy.ps1
@@ -1,3 +1,5 @@
 Write-Output  ("administratorLogin: {0}" -f ${Env:administratorLogin})
 Write-Output  ("administratorLoginPassword: {0}" -f ${Env:administratorLoginPassword})
 Write-Output  ("storageAccountKey: {0}" -f ${Env:storageAccountKey})
+
+dir env:
