9c52ea40dee985ce2eb23fcb42214e512e0c2068
bramplouvier <bramplouvier@gmail.com>
2022-07-24 12:09:49 +0200

    Update setupSampleDatabase.sh

--- infra/setupSampleDatabase.sh
+++ infra/setupSampleDatabase.sh
@@ -12,18 +12,18 @@ az storage blob upload-batch \
     --account-name $storageAccountName \
     --overwrite
     
-sleep 5
+#sleep 5
 
-az sql db import \
-        --admin-password $administratorLoginPassword \
-        --admin-user $administratorLogin \
-        --storage-key $storageAccountKey \
-        --storage-key-type StorageAccessKey \
-        --storage-uri https://$storageAccountName.blob.core.windows.net/bacpacs/WideWorldImporters-Standard.bacpac \
-        --name wwi \
-        --auth-type SQL \
-        --resource-group $resourceGroup \
-        --server $sqlServerName
+#az sql db import \
+#        --admin-password $administratorLoginPassword \
+#        --admin-user $administratorLogin \
+#        --storage-key $storageAccountKey \
+#        --storage-key-type StorageAccessKey \
+#        --storage-uri https://$storageAccountName.blob.core.windows.net/bacpacs/WideWorldImporters-Standard.bacpac \
+#        --name wwi \
+#        --auth-type SQL \
+#        --resource-group $resourceGroup \
+#        --server $sqlServerName
 
-echo "done"
-return 1
+#echo "done"
+#return 1
