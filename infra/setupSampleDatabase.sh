3bd9f1c207e97813da95abc1eeb21e24ab9576e5
Bram Plouvier <bram@dxa.nl>
2024-01-20 17:45:24 +0100

    Remove timeout

--- infra/setupSampleDatabase.sh
+++ infra/setupSampleDatabase.sh
@@ -14,8 +14,7 @@ az storage blob upload-batch \
     --destination "bacpacs" \
     --overwrite
 
-# Use timeout to prevent deployment from waiting on import
-timeout 10 az sql db import \
+az sql db import \
         --admin-password $administratorLoginPassword \
         --admin-user $administratorLogin \
         --storage-key $ACCOUNT_KEY \
@@ -25,10 +24,3 @@ timeout 10 az sql db import \
         --auth-type SQL \
         --resource-group $resourceGroup \
         --server $sqlServerName
-
-RC=$?
-if [[ $RC -eq 143 ]]; then
-  exit 0
-else
-  exit $RC
-fi
